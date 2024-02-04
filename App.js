d5f6f5ab7d8ef1f753934b00349e8a3ca48ca09f
chethan <tech.ck16@gmail.com>
2024-02-04 18:26:20 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,27 @@
+const parent = React.createElement(
+    "div", // element type
+    {id: "parent"}, // attributes
+    React.createElement(
+        "div",
+        {id: "child"},
+        [
+            React.createElement(
+                "h1",
+                {id: 1},
+                "I am an H1 Tag!!"
+            ),
+            React.createElement(
+                "h2",
+                {id: 2},
+                "I am an H2 Tag!!"
+            )
+        ]
+    )
+);  
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const header = ReactDOM.createRoot(document.getElementById("header"));
+root.render(parent);
+header.render(heading);
